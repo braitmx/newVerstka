c0ab8bd7e135553275c69929de554fc562795cf3
Alex S <Braitmx@gmail.com>
2017-09-27 13:47:27 +0300

    update google map

--- gitex/js/head_menu.js
+++ gitex/js/head_menu.js
@@ -1,4 +1,4 @@
 let touch_menu = document.querySelector(".header__touch-menu"),
-    header_menu = document.querySelector(".header__menu");
+    menu = document.querySelector(".header__menu");
 
-touch_menu.addEventListener('click', () => header_menu.classList.toggle('active'));
+touch_menu.addEventListener('click', () => menu.classList.toggle('active'));
--- gitex/js/map.js
+++ gitex/js/map.js
@@ -1,18 +1,146 @@
-function initMap() {
-    var map = new google.maps.Map(document.querySelector('.map'), {
-        zoom: 6,
-        center: {
-            lat: 40,
-            lng: -73
-        }
-    });
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+
+    var mapOptions = {
+
+        zoom: 12,
+        center: new google.maps.LatLng(40.895, -73.881),
+
+        styles: [
+            {
+                "featureType": "administrative",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "-100"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 65
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": "50"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "-100"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "lightness": "30"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "lightness": "40"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffff00"
+                    },
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -97
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -100
+                    }
+                ]
+            }
+        ]
+    };
+
+    var mapElement = document.querySelector('.map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+
 
     var image = './images/marker.png';
-    var beachMarker = new google.maps.Marker({
-        position: {
-            lat: 40.895,
-            lng: -73.881
-        },
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.895, -73.881),
         map: map,
         icon: image
     });
